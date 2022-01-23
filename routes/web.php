851e2b8f14fcd3f20e343a1f6be071b7bfb3f084
WolflanFreitas <wolflanf@gmail.com>
2022-01-23 09:05:36 -0300

    Ok

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['middleware' => ['auth'], 'namespace' => 'Admin'], function() {
+
+    Route::get('admin','AdminController@index')->name('admin.home');
 });
+
+Route::get('/','Site\SiteController@index')->name('home');
+
+
 
 Auth::routes();
 
